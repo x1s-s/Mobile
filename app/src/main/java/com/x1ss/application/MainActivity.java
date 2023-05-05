53ced022ed5b81b8903d2aaecf8bd58894cb3026
x1ss <shaleprog@gmail.com>
2023-05-05 14:43:43 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -15,40 +15,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void clockwise(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
-        image.startAnimation(animation);
-    }
-
-    public void zoom(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanimation);
-        image.startAnimation(animation);
-    }
-
-    public void blink(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
-        image.startAnimation(animation);
-    }
-
-    public void move(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
-        image.startAnimation(animation);
-    }
-
-    public void slide(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
-        image.startAnimation(animation);
-    }
-
-    public void fade(View view) {
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
-        image.startAnimation(animation);
-    }
 }
