42858dfb754eb9c015390eeb8c23e37ce9d573dc
x1ss <shaleprog@gmail.com>
2023-05-08 19:03:33 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -4,15 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView;
+    MultiAutoCompleteTextView multiAutoCompleteTextView;
+    String[] languages = {"Android ", "java", "IOS", "SQL", "JDBC", "Web services"};
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, languages);
+
+        //auto complete text view
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(adapter);
+
+        //multi auto complete text view
+        multiAutoCompleteTextView = findViewById(R.id.multiAutoCompleteTextView1);
+        multiAutoCompleteTextView.setThreshold(1);
+        multiAutoCompleteTextView.setAdapter(adapter);
+        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
 }
