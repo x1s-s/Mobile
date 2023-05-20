3cca6e6c090c1d42339cd9702704fc41aebe9ca4
x1ss <shaleprog@gmail.com>
2023-05-20 16:39:27 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -1,15 +1,43 @@
 package com.x1ss.application;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv;
+    private Matrix matrix = new Matrix();
+    private float scale = 1f;
+    private ScaleGestureDetector SGD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println();
         setContentView(R.layout.activity_main);
+
+        iv=(ImageView)findViewById(R.id.imageView);
+        SGD = new ScaleGestureDetector(this,new ScaleListener());
+    }
+
+    public boolean onTouchEvent(MotionEvent ev) {
+        SGD.onTouchEvent(ev);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.
+            SimpleOnScaleGestureListener {
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+            matrix.setScale(scale, scale);
+            iv.setImageMatrix(matrix);
+            return true;
+        }
     }
 }
