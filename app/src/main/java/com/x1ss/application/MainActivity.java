e4e84a9ff2c3a91775fc67778bc19ab239e7faec
x1ss <shaleprog@gmail.com>
2023-05-20 17:01:48 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -6,13 +6,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+    TextView tv;
+    EditText ed1;
+
+    String data;
+    private final String file = "mydata";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+
+        ed1 = findViewById(R.id.editText);
+        tv = findViewById(R.id.textView2);
+        b1.setOnClickListener(v -> {
+            data = ed1.getText().toString();
+            try {
+                FileOutputStream fOut = openFileOutput(file, MODE_PRIVATE);
+                fOut.write(data.getBytes());
+                fOut.close();
+                Toast.makeText(getBaseContext(), "file saved", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+
+        b2.setOnClickListener(v -> {
+            try {
+                FileInputStream fin = openFileInput(file);
+                int c;
+                StringBuilder temp = new StringBuilder();
+                while ((c = fin.read()) != -1) {
+                    temp.append((char) c);
+                }
+                tv.setText(temp.toString());
+                Toast.makeText(getBaseContext(), "file read", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
