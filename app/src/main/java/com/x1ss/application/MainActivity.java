40fb196a8d2c591eb3ed4aa71191257e048ae5dd
x1ss <shaleprog@gmail.com>
2023-05-20 17:26:59 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -1,18 +1,26 @@
 package com.x1ss.application;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
+        spinner = findViewById(R.id.progressBar);
+        spinner.setVisibility(View.GONE);
+
+        b1.setOnClickListener(v -> spinner.setVisibility(View.VISIBLE));
     }
 }
