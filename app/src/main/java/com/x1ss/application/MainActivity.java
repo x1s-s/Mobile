5f4234062aa2f9ebc9cba4f800cc179a438c4a5b
x1ss <shaleprog@gmail.com>
2023-05-20 17:36:26 +0300

    +

--- app/src/main/java/com/x1ss/application/MainActivity.java
+++ app/src/main/java/com/x1ss/application/MainActivity.java
@@ -2,17 +2,56 @@ package com.x1ss.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+    EditText ed1, ed2;
+
+    TextView tx1;
+    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
+        ed1 = findViewById(R.id.editText);
+        ed2 = findViewById(R.id.editText2);
+
+        b2 = findViewById(R.id.button2);
+        tx1 = findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(v -> {
+            if (ed1.getText().toString().equals("admin") &&
+                    ed2.getText().toString().equals("admin")) {
+                Toast.makeText(getApplicationContext(),
+                        "Redirecting...", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                        tx1.setVisibility(View.VISIBLE);
+                tx1.setBackgroundColor(Color.RED);
+                counter--;
+                tx1.setText(Integer.toString(counter));
+
+                if (counter == 0) {
+                    b1.setEnabled(false);
+                }
+            }
+        });
+
+        b2.setOnClickListener(v -> finish());
     }
 }
